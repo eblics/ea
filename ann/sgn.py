e48347db4c993453273d7e5eb5d0844dd98ef6af
eblics <eblics@gmail.com>
2018-04-14 15:21:35 +0800

    sgn

--- ann/sgn.py
+++ ann/sgn.py
@@ -17,21 +17,41 @@ np.set_printoptions(threshold=np.inf)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # tf.reset_default_graph()
-NTRADERS=100
-NHIDDEN=13
+NTRADERS=3
+NHIDDEN=3
 INITIAL_FOUNDS=1000.0
-PERIOD=55
+PERIOD=5
 LOTS=1
-GAP=100.0
+GAP=10.0
 GAP_FLOAT=0.00001*GAP
 # FACTOR=1000000
 FACTOR=np.exp(8)
 EPOCH=100
 
 
+with tf.variable_scope('sgn',reuse=tf.AUTO_REUSE):
+    initializer=tf.random_normal_initializer(mean=0, stddev=1)
+    w1_var=tf.get_variable(name='w1',shape=[NTRADERS,NHIDDEN,PERIOD],initializer=initializer)
+    b1_var=tf.get_variable(name='b1',shape=[NTRADERS,NHIDDEN,PERIOD],initializer=initializer)
+    w2_var=tf.get_variable(name='w2',shape=[NTRADERS,NHIDDEN,NHIDDEN],initializer=initializer)
+    b2_var=tf.get_variable(name='b2',shape=[NTRADERS,NHIDDEN,NHIDDEN],initializer=initializer)
+    w3_var=tf.get_variable(name='w3',shape=[NTRADERS,3,NHIDDEN],initializer=initializer)
+    blances_var=tf.get_variable(name='blances',shape=[NTRADERS],initializer=tf.constant_initializer(INITIAL_FOUNDS))
+    order_var=tf.get_variable(name='order',shape=[NTRADERS],dtype=tf.int32,initializer=tf.constant_initializer(-1))
+    oop_var=tf.get_variable(name='oop',shape=[NTRADERS],initializer=tf.constant_initializer(0))
 
-def succ(x,opv,ov,bv):
-    price=dclose[x]
+open_ph=tf.placeholder(shape=[None],dtype=tf.float32,name='open')
+close_ph=tf.placeholder(shape=[None],dtype=tf.float32,name='close')
+
+def succ(x,bv,ov,opv):
+    # with tf.variable_scope('sgn',reuse=True):
+        # ov=tf.get_variable(name='order',dtype=tf.int32)
+        # opv=tf.get_variable(name='oop')
+        # bv=tf.get_variable(name='blances')
+
+    # bv=tf.Print(bv,[bv],message='succ:')
+    # x=tf.Print(x,[x],message="body:")
+    price=close_ph[x]
     bmask=tf.cast(tf.equal(ov,0),tf.float32)
     bop=opv*bmask
     profit_ovs=tf.cast(tf.less_equal(bop,price-GAP_FLOAT),tf.int32)
@@ -56,14 +76,15 @@ def succ(x,opv,ov,bv):
     bv=bv-tf.cast(loss_ovs,tf.float32)*LOTS*GAP
     # bv=tf.Print(bv,[bv])
 
-    s=dclose[x-PERIOD+1:x+1]-dopen[x-PERIOD+1:x+1]
-    y=decide5(s)
+    s=close_ph[x-PERIOD+1:x+1]-open_ph[x-PERIOD+1:x+1]
+    y=decide(s)
     ov=ov+tf.cast(tf.equal(ov,-1),tf.int32)*tf.argmax(y,1,output_type=tf.int32)
     cp=tf.cast(tf.equal(opv,0),tf.float32)*price
     opv=opv+cp
     opv=tf.cast(tf.greater(bv,0),tf.float32)*opv
-    # bv=tf.Print(bv,[bv],message='bv:',summarize=20)
-    return x,opv,ov,bv
+    # with tf.variable_scope('sgn',reuse=True):
+        # bv=tf.assign(tf.get_variable(name='blances'),bv)
+    return x,bv,ov,opv
 
 def decide(s):
     y=w1_var*s*FACTOR+b1_var
@@ -113,60 +134,88 @@ def decide5(s):
     y=tf.nn.softmax(y)
     return y
 
-def decide6(s):
-    # y=tf.nn.elu(s)
-    y=w4_var*s*FACTOR
-    y=tf.nn.sigmoid(y)
-    y=tf.reduce_sum(y,axis=2)
-    y=tf.nn.softmax(y)
-    return y
+#def decide6(s):
+#    # y=tf.nn.elu(s)
+#    y=w4_var*s*FACTOR
+#    y=tf.nn.sigmoid(y)
+#    y=tf.reduce_sum(y,axis=2)
+#    y=tf.nn.softmax(y)
+#    return y
 
-def fail(x,opv,ov,bv):
-    return x,opv,ov,bv
+def fail(x,bv,ov,opv):
+    return x,bv,ov,opv
 
-def cond(x,opv,ov,bv):
-    return tf.logical_and(x<tf.size(dclose),tf.reduce_any(tf.greater(bv,0)))
+def cond(i,j,bv,ov,opv):
+    with tf.variable_scope('sgn',reuse=tf.AUTO_REUSE):
+        bv=tf.get_variable('blances')
+    # return tf.logical_and(x<tf.size(close_ph),tf.reduce_any(tf.greater(bv,0)))
+    # x=tf.Print(x,[x],message='body')
+    return j<tf.size(close_ph)
 
-def body(x,opv,ov,bv):
-    x,opv,ov,bv=tf.cond(x>=PERIOD-1,
-        lambda:succ(x,opv,ov,bv),
-        lambda:fail(x,opv,ov,bv))
-    x+=1;#x=tf.Print(x,[x])
-    return x,opv,ov,bv
-# mean=tf.reduce_mean(blances_var)
-blances=np.full((NTRADERS,),INITIAL_FOUNDS,dtype=np.float32)
-w1=np.random.randn(NTRADERS,NHIDDEN,PERIOD)
-b1=np.random.randn(NTRADERS,NHIDDEN,PERIOD)
-w2=np.random.randn(NTRADERS,NHIDDEN,NHIDDEN)
-b2=np.random.randn(NTRADERS,NHIDDEN,NHIDDEN)
-w3=np.random.randn(NTRADERS,3,NHIDDEN)
-w4=np.random.randn(NTRADERS,3,PERIOD)
-#w1=w1*(w1<0.5)/np.sqrt(NTRADERS*PERIOD)
-#b1=b1*(b1<0.5)/np.sqrt(NTRADERS*PERIOD)
-#w2=w2*(w2<0.5)/np.sqrt(NTRADERS*PERIOD)
-#b2=b2*(b2<0.5)/np.sqrt(NTRADERS*PERIOD)
-#w3=w3*(w3<0.5)/np.sqrt(NTRADERS*PERIOD)
-norder=np.random.randint(-1,0,(NTRADERS))
-noop=np.zeros((NTRADERS),dtype=np.float32)
+def body(i,j,bv,ov,opv):
+    j,bv,ov,opv=tf.cond(j>=PERIOD-1,
+        lambda:succ(j,bv,ov,opv),
+        lambda:fail(j,bv,ov,opv))
+    j+=1;
+    # j=tf.Print(j,[j],message='body')
+    return i,j,bv,ov,opv
 
-blances_var=tf.placeholder(shape=blances.shape,dtype=np.float32,name='blances')
-w1_var=tf.placeholder(shape=w1.shape,dtype=tf.float32,name='w1')
-b1_var=tf.placeholder(shape=b1.shape,dtype=tf.float32,name='b1')
-w2_var=tf.placeholder(shape=w2.shape,dtype=tf.float32,name='w2')
-b2_var=tf.placeholder(shape=b2.shape,dtype=tf.float32,name='b2')
-w3_var=tf.placeholder(shape=w3.shape,dtype=tf.float32,name='w3')
-w4_var=tf.placeholder(shape=w4.shape,dtype=tf.float32,name='w4')
+def ajust_body(k,m,prob):
+    k=tf.cond(tf.not_equal(k,m),lambda:ajust(k,m,prob),lambda:k)
+    k+=1
+    return k,m,prob
 
-dopen=tf.placeholder(shape=[None],dtype=tf.float32,name='open')
-dclose=tf.placeholder(shape=[None],dtype=tf.float32,name='close')
-order=tf.placeholder(shape=norder.shape,dtype=tf.int32,name='order')
-oop=tf.placeholder(shape=noop.shape,dtype=np.float32,name='oop')
+def ajust(k,m,prob):
+    with tf.variable_scope('sgn',reuse=tf.AUTO_REUSE):
+        w1v=tf.get_variable(name='w1')
+        b1v=tf.get_variable(name='b1')
+        w2v=tf.get_variable(name='w2')
+        b2v=tf.get_variable(name='b2')
+        w3v=tf.get_variable(name='w3')
+
+    tf.assign(w1v[k],w1v[k]*0.5+w1v[m]*0.5)
+    tf.assign(w1v[k],tf.nn.dropout(w1v[k],prob))
+    tf.assign(b1v[k],b1v[k]*0.5+b1v[m]*0.5,prob)
+    tf.assign(b1v[k],tf.nn.dropout(b1v[k],prob))
+    tf.assign(w2v[k],w2v[k]*0.5+w2v[m]*0.5,prob)
+    tf.assign(w2v[k],tf.nn.dropout(w2v[k],prob))
+    tf.assign(b2v[k],b2v[k]*0.5+b2v[m]*0.5)
+    tf.assign(b2v[k],tf.nn.dropout(b2v[k],prob))
+    tf.assign(w3v[k],w3v[k]*0.5+w3v[m]*0.5)
+    tf.assign(w3v[k],tf.nn.dropout(w3v[k],prob))
+    return k
+
+def main_body(i,prob):
+    with tf.variable_scope('sgn',reuse=True):
+        ov=tf.get_variable(name='order',dtype=tf.int32)
+        opv=tf.get_variable(name='oop')
+        bv=tf.get_variable(name='blances')
+        # bv=tf.assign(tf.get_variable('blances'),tf.ones([NTRADERS])*INITIAL_FOUNDS)
+        # ov=tf.assign(tf.get_variable('order',dtype=tf.int32),tf.ones([NTRADERS],dtype=tf.int32)*-1)
+        # opv=tf.assign(tf.get_variable('oop'),tf.zeros([NTRADERS],dtype=tf.float32))
+    j=tf.constant(0)
+    i,j,bv,ov,opv=tf.while_loop(cond,body,[i,j,bv,ov,opv])
+    # bv=tf.Print(bv,[bv],'main:')
+    # with tf.variable_scope('sgn',reuse=True):
+        # bv=tf.assign(tf.get_variable('blances'),tf.ones_like(bv)*INITIAL_FOUNDS)
+        # ov=tf.assign(tf.get_variable('order',dtype=tf.int32),tf.ones_like(ov,dtype=tf.int32)*-1)
+        # opv=tf.assign(tf.get_variable('oop'),tf.zeros_like(ov,dtype=tf.float32))
+    m=tf.argmax(bv,output_type=tf.int32)
+    k=tf.constant(0)
+    k,m,prob=tf.while_loop(lambda k,m,p:k<NTRADERS,ajust_body,[k,m,prob])
+    i+=1
+    # i=tf.Print(i,[i],message='main:')
+    prob=tf.cond(prob<0.99,lambda:prob+0.01,lambda:prob)
+    return i,prob
+
+
 loop_i=tf.constant(0)
-loop=tf.while_loop(cond,body,[loop_i,oop,order,blances_var])
+prob=tf.constant(0.5)
+main_loop=tf.while_loop(lambda x,p:x<EPOCH,main_body,[loop_i,prob])
+# loop=tf.while_loop(cond,body,[loop_i])
 
 DUMP_PATH='winner.sgn'
 def train(fname):
-    global loop,blances,norder,noop,w1,w2,b1,b2,w3,blances_var,order,oop,w1_var,w2_var,b1_var,b2_var,w3_var
     if os.path.exists(DUMP_PATH):
         with open(DUMP_PATH,'rb') as f:
             bm,w1,b1,w2,b2,w3=pickle.load(f)
@@ -176,47 +225,24 @@ def train(fname):
     xo=np.array(df['open'],dtype=np.float32)
     xc=np.array(df['close'],dtype=np.float32)
     sess=tf.Session()
-    for e in range(0,EPOCH):
-        sess.run(tf.global_variables_initializer())
-        x,opv,ov,bv=sess.run(loop,feed_dict={dopen:xo,dclose:xc,w1_var:w1,b1_var:b1,w2_var:w2,b2_var:b2,w3_var:w3,w4_var:w4,oop:noop,order:norder,blances_var:blances})
-        prop=0.5
-        m=np.argmax(bv)
-        print('epoch:%d max:%f argmax:%d m:%d'%(e,bv.max(),bv.argmax(),m))
-        with open(DUMP_PATH,'wb') as f:
-            pickle.dump((m,w1,b1,w2,b2,w3),f)
-        for i in range(0,NTRADERS):
-            if i==m:continue
-            if random.random()>prop:
-                w1[i]=np.random.randn(w1[i].shape[0],w1[i].shape[1])
-                b1[i]=np.random.randn(b1[i].shape[0],b1[i].shape[1])
-                w2[i]=np.random.randn(w2[i].shape[0],w2[i].shape[1])
-                b2[i]=np.random.randn(b2[i].shape[0],b2[i].shape[1])
-                w3[i]=np.random.randn(w3[i].shape[0],w3[i].shape[1])
-                w4[i]=np.random.randn(w4[i].shape[0],w4[i].shape[1])
-                if prop<0.99:prop+=0.001
-            w1[i]=w1[m]*0.5+w1[i]*0.5
-            b1[i]=b1[m]*0.5+b1[i]*0.5
-            w2[i]=w2[m]*0.5+w2[i]*0.5
-            b2[i]=b2[m]*0.5+b2[i]*0.5
-            w3[i]=w3[m]*0.5+w3[i]*0.5
-            w4[i]=w4[m]*0.5+w4[i]*0.5
-        blances=np.full((NTRADERS,),INITIAL_FOUNDS,dtype=np.float32)
-        norder=np.random.randint(-1,0,(NTRADERS))
-        noop=np.zeros((NTRADERS),dtype=np.float32)
-        # x,opv,ov,bv=sess.run(loop,feed_dict={open:xo,close:xc,w1_var:w1,b1_var:b1,w2_var:w2,b2_var:b2,w3_var:w3,oop:noop,order:norder,blances_var:blances})
+    sess.run(tf.global_variables_initializer())
+    sess.run(main_loop,feed_dict={open_ph:xo,close_ph:xc})
+    print(sess.run(blances_var))
+    # with open(DUMP_PATH,'wb') as f:
+        # pickle.dump((m,w1,b1,w2,b2,w3),f)
 
 def pred(fname):
     global loop,blances,norder,noop,w1,w2,b2,w3,blances_var,order,oop,w1_var,w2_var,b1_var,b2_var,w3_var
     if os.path.exists(DUMP_PATH):
         with open(DUMP_PATH,'rb') as f:
-            bm,w1,w3=pickle.load(f)
+            bm,w1,b1,w2,b2,w3=pickle.load(f)
     df=pd.read_csv(fname,header=None)
     df.columns=['date','time','open','high','low','close','volume']
     xo=np.array(df['open'],dtype=np.float32)
     xc=np.array(df['close'],dtype=np.float32)
     sess=tf.Session()
     sess.run(tf.global_variables_initializer())
-    x,opv,ov,bv=sess.run(loop,feed_dict={dopen:xo,dclose:xc,w1_var:w1,b1_var:b1,w2_var:w2,b2_var:b2,w3_var:w3,w4_var:w4,oop:noop,order:norder,blances_var:blances})
+    x,opv,ov,bv=sess.run(loop,feed_dict={open_ph:xo,close_ph:xc,w1_var:w1,b1_var:b1,w2_var:w2,b2_var:b2,w3_var:w3,w4_var:w4,oop:noop,order:norder,blances_var:blances})
     print('argmax:%f maxv:%f m:%d mv:%f'%(bv.argmax(),bv.max(),bm,bv[bm]))
 
 if sys.argv[1]=='train':
