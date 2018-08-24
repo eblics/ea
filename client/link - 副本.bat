f66efbb60b8679a28f511a26c55b953a66451aed
eblics <eblic@gmail.com>
2018-08-24 15:16:53 +0800

    d ddf

--- client/link - 副本.bat
+++ client/link - 副本.bat
@@ -1,4 +1,8 @@
 set prj_path=%~dp0
-set mql_path=C:\Users\eblics\AppData\Roaming\MetaQuotes\Terminal\F5C18A2156882613427FB4ACF0892997\MQL4\
-mklink /j %mql_path%\Experts\ea %prj_path%Experts 
-mklink /j %mql_path%\Indicators\ea %prj_path%\Indicators 
+set mql_path=C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\F5C18A2156882613427FB4ACF0892997\MQL4\
+mklink /j %mql_path%Experts\ea %prj_path%Experts\ 
+mklink /j %mql_path%Indicators\ea %prj_path%\Indicators\ 
+
+set mql_path=C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\9A651CFCBD45340C89F4AF43884432ED\MQL4\
+mklink /j %mql_path%Experts\ea %prj_path%Experts\ 
+mklink /j %mql_path%Indicators\ea %prj_path%Indicators\
